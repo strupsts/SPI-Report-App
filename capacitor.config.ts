d46b13d8e9b4064b9f41b2cdd1fa201f2d47194f
Strupsts <strupsts@mail.ru>
2024-05-31 19:57:31 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SPI-Report-App',
+  webDir: 'www'
+};
+
+export default config;
